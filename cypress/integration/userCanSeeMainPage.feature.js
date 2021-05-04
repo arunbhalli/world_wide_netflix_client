765cd486fffc31cc110ab55fb7291243f10a7ae7
luke <lukeperrin27@gmail.com>
2021-05-04 21:19:14 +0200

    adds footer functional component

--- cypress/integration/userCanSeeMainPage.feature.js
+++ cypress/integration/userCanSeeMainPage.feature.js
@@ -6,6 +6,10 @@ describe('User can see main page', () => {
   it('is expected to show the header', () => {
     cy.get('[data-cy=header]').should('exist');
   });
+
+  it('is expected to show the footer', () => {
+    cy.get('[data-cy=footer]').should('exist');
+  });
 })
 
 
---
+++ src/component/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer data-cy="footer">
+      Basic site information
+    </footer>
+  )
+}
+
+export default Footer
