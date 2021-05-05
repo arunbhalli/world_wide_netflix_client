42769bda19767d75fb91542fa956ff8bb45e9b6a
luke <lukeperrin27@gmail.com>
2021-05-05 10:23:57 +0200

    adds tests for registration form

--- cypress/integration/userCanSeeMainPage.feature.js
+++ cypress/integration/userCanSeeMainPage.feature.js
@@ -1,35 +1,43 @@
 describe('User can see main page', () => {
-  before(() => 
-  cy.visit('/')
-  )
+	before(() => cy.visit('/'))
 
-  it('is expected to show the header', () => {
-    cy.get('[data-cy=header]').should('exist');
-  });
+	it('is expected to show the header', () => {
+		cy.get('[data-cy=header]').should('exist')
+	})
 
-  it('is expected to show the footer', () => {
-    cy.get('[data-cy=footer]').should('exist');
-  });
+	it('is expected to show the footer', () => {
+		cy.get('[data-cy=footer]').should('exist')
+	})
 
-  it('is expected to show the login button', () => {
-    cy.get('[data-cy=login-btn]').should('contain', 'Login');
-  });
+	it('is expected to show the login button', () => {
+		cy.get('[data-cy=login-btn]').should('contain', 'Login')
+	})
 })
 
 describe('User can click on login button and login modal appears', () => {
-  it('is expected to show login modal', () => {
-    cy.get('[data-cy=login-btn]').click();
-    cy.get('[data-cy=login-modal-header]').should('be.visible');
-    cy.get('[data-cy=login-modal-content]').within(() => {
-      cy.get('[data-cy=login-email-input]').type('bob.kramer@hotmail.com')
-      cy.get('[data-cy=login-password]').type('password')
-      cy.get('[data-cy=form-login-btn]').click()
-    })
-  });
+	it('is expected to show login modal', () => {
+		cy.get('[data-cy=login-btn]').click()
+		cy.get('[data-cy=login-modal-header]').should('be.visible')
+		cy.get('[data-cy=login-modal-content]').within(() => {
+			cy.get('[data-cy=login-email-input]').type('bob.kramer@hotmail.com')
+			cy.get('[data-cy=login-password]').type('password')
+			cy.get('[data-cy=form-login-btn]').click()
+		})
+	})
 })
 
-
-
-
-
-
+describe('User can see register form', () => {
+	it('is expected to show the registration form', () => {
+		cy.get('[data-cy=login-btn]').click()
+		cy.get('[data-cy=login-modal-header]').should('be.visible')
+		cy.get('[data-cy=login-modal-content]').within(() => {
+			cy.get('[data-cy=registration-email-input]').type(
+				'bob.kramer@hotmail.com'
+			)
+			cy.get('[data-cy=user-country]').select('Sweden')
+			cy.get('[data-cy=registration-password]').type('password')
+			cy.get('[data-cy=registration-confirmation-password]').type('password')
+			cy.get('[data-cy=form-register-btn]').click()
+		})
+	})
+})
--- src/component/LoginModal.jsx
+++ src/component/LoginModal.jsx
@@ -1,43 +1,56 @@
-import React, { useState } from "react";
-import { Modal, Button, Form } from "semantic-ui-react";
+import React, { useState } from 'react'
+import { Modal, Button, Form } from 'semantic-ui-react'
 
 const LoginModal = () => {
+	const [visibility, setVisibility] = useState(false)
+	return (
+		<Modal
+			centered={false}
+			open={visibility}
+			onClose={() => setVisibility(false)}
+			onOpen={() => setVisibility(true)}
+			trigger={<Button data-cy='login-btn'>Login</Button>}>
+			<Modal.Header data-cy='login-modal-header'>Login</Modal.Header>
+			<Modal.Content data-cy='login-modal-content'>
 
-  const [visibility, setVisibility] = useState(false);
-  return (
-    <Modal
-      centered={false}
-      open={visibility}
-      onClose={() => setVisibility(false)}
-      onOpen={() => setVisibility(true)}
-      trigger={<Button data-cy="login-btn">Login</Button>}
-    >
-      <Modal.Header data-cy="login-modal-header">Login</Modal.Header>
-      <Modal.Content data-cy='login-modal-content'>
-        <Form>
-          <Form.Group widths='equal'>
-            <Form.Input 
-              fluid type='email'
-              label='email'
-              placeholder='email' 
-              data-cy='login-email-input' 
-              required/>
-            <Form.Input 
-              fluid type='password' 
-              label='Password' 
-              placeholder='Password' 
-              data-cy='login-password' 
-              required/>
-          </Form.Group>
-          <Button type='submit' data-cy='form-login-btn' 
-          onClick={() => setVisibility(false)}>Login</Button>
-        </Form>
-      </Modal.Content>
-      <Modal.Actions>
-        <Button onClick={() => setVisibility(false)}>Close</Button>
-      </Modal.Actions>
-    </Modal>
-  );
-};
+    
 
-export default LoginModal;
+
+
+
+
+				<Form>
+					<Form.Group widths='equal'>
+						<Form.Input
+							fluid
+							type='email'
+							label='email'
+							placeholder='email'
+							data-cy='login-email-input'
+							required
+						/>
+						<Form.Input
+							fluid
+							type='password'
+							label='Password'
+							placeholder='Password'
+							data-cy='login-password'
+							required
+						/>
+					</Form.Group>
+					<Button
+						type='submit'
+						data-cy='form-login-btn'
+						onClick={() => setVisibility(false)}>
+						Login
+					</Button>
+				</Form>
+			</Modal.Content>
+			<Modal.Actions>
+				<Button onClick={() => setVisibility(false)}>Close</Button>
+			</Modal.Actions>
+		</Modal>
+	)
+}
+
+export default LoginModal
