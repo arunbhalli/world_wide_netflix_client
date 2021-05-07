00ddddf71921fe22decf394948e7d914b4bd5b1c
DavDan1 <david-danielyan@hotmail.com>
2021-05-07 09:50:33 +0200

    WIP add module for geolocations

---
+++ src/modules/getUserLocation.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+
+const getUserLocation = () => {
+  const [latitude, setLatitude] = useState()
+  const [longitude, setLongitude] = useState()
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default getUserLocation;
