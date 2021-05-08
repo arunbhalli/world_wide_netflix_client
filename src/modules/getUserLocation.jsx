873a235fbd29385b5c168810bea80c12199d2ff5
kim_haaga <kim.haaga@live.se>
2021-05-08 16:33:39 +0200

    WIP Fix issues with getting coordinates

--- src/modules/getUserLocation.jsx
+++ src/modules/getUserLocation.jsx
@@ -1,11 +1,11 @@
-export const getUserLocation = async () => {
-  const coords = await window.navigator.geolocation.getCurrentPosition(
-    (position) => {
-      return position.coords;
-    }
-  );
-  if (coords) {
-    return [coords.latitude, coords.longitude];
-  }
-  return [null, null];
-};
+// export const getUserLocation = async () => {
+//   const coords = await window.navigator.geolocation.getCurrentPosition(
+//     (position) => {
+//       return position.coords;
+//     }
+//   );
+//   if (coords) {
+//     return [coords.latitude, coords.longitude];
+//   }
+//   return [null, null];
+// };
