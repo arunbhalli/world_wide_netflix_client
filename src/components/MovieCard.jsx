9ef9151f7e24eac7f8c947c767a538d11b277906
sevader14 <vsevolod.deriushkin@gmail.com>
2021-05-09 22:21:42 +0200

    adds country flags to MovieCards

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import he from 'he';
 import _ from 'lodash';
-import { Card, Image } from 'semantic-ui-react';
+import { Card, Flag, Image } from 'semantic-ui-react';
 
 const MovieCard = ({ movie, i }) => {
+  let countryArray = Object.keys(JSON.parse('{' + movie.clist + '}'))
+  
+  if (countryArray.length > 10) {
+    countryArray.pop() 
+  }
+  
+  let countryList = countryArray.map((country) => {
+    return <Flag name={_.toLower(country)}/>
+  })
+  
   return (
     <Card data-cy={`movie-${i}`}>
       <Image src={movie.img} />
@@ -13,6 +23,7 @@ const MovieCard = ({ movie, i }) => {
         </Card.Header>
         <Card.Description>
           Rating: {_.round(movie.avgrating, 1)}
+          Availible in: {countryList}
         </Card.Description>
       </Card.Content>
     </Card>
